e1a89b71157a159c65db21f65cbdfb5a52d6931e
nspp99b <chromepanthers@gmail.com>
2018-01-30 13:56:59 -0500

    defined components with event handlers

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,22 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  createCoordinates = (e) => this.props.onReceiveCoordinates([e.screenX, e.screenY])
+    // e.preventDefault()
+    // // console.log(e)
+    // // let eventData = e.target
+    // let coords = [e.screenX, e.screenY]
+    // console.log(coords)
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.createCoordinates}>Coordinator</button>
+      </div>
+    )
+  }
+}
+
+export default CoordinatesButton;
